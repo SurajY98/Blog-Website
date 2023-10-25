7341b3bef64e8b870d123818898fbce6793974c6
Suraj Yadav <78532737+SurajY98@users.noreply.github.com>
2023-10-26 01:21:31 +0530

    Add files via upload

---
+++ Blog-App/src/test/java/com/suraj/blogapp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
